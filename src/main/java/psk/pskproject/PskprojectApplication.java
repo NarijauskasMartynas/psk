e0c4e8b8aff9368d2e2ba67264ed70b98fd4b83d
Martynas <narijauskasmartynas@gmail.com>
2019-03-18 22:24:08 +0200

    Initialized project

---
+++ src/main/java/psk/pskproject/PskprojectApplication.java
@@ -0,0 +1,13 @@
+package psk.pskproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PskprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PskprojectApplication.class, args);
+    }
+
+}
